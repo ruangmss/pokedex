b707c853683038801a5b7666d3b53d03a7e8039b
Ruan Gomes <ruan.gmss@outlook.com>
2026-07-23 14:02:45 -0300

    feat: Estruturação inicial de rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,20 @@
-function App() {
-  return <div>Teste</div>;
-}
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Pokemon from './pages/Pokemon';
+import NotFound from './pages/NotFound';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="pokemon/:name" element={<Pokemon />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
