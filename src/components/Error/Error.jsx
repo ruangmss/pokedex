6ef94f038c7b808e521891e8d8d311ca4483a28c
Ruan Gomes <ruan.gmss@outlook.com>
2026-07-31 15:54:45 -0300

    fix: Ajuste de UI no componente de erro

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -2,7 +2,7 @@ import './Error.css';
 
 const Error = ({ error }) => {
   return (
-    <div className="error-container">
+    <div className="error-container container">
       <div className="error-box">
         <h2>Ops! Algo deu errado.</h2>
         <p>{error}</p>
